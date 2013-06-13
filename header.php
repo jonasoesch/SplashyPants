bbf17be1933c42745ef5011d6ac3ed897c43e6ff
Jonas Oesch <jonas.oesch@heig-vd.ch>
2013-06-13 16:00:52 +0200

    Initial commit

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Splashy Title</title>
+      <link href="css/grid.css" rel="stylesheet" />
+      <link href="css/normalize.css" rel="stylesheet" />
+    </head>
+    <body>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include 'header.php'; ?>
+
+<p>Hello Splashy</p>
+
+<?php include 'footer.php'; ?>
