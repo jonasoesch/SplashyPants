9c6c98fbf1a162f382cdd0a6bf24d29ce7e1253f
Jonas Oesch <jonas.oesch@heig-vd.ch>
2013-06-13 21:38:45 +0200

    Commencé une base typographique.
    typography.less contient une base typographique.
    Ajouteé des exemples dans le index.

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
   <html>
     <head>
+      <meta charset="utf-8">
       <title>Splashy Title</title>
       <link href="css/grid.css" rel="stylesheet" />
       <link href="css/normalize.css" rel="stylesheet" />
-            <link href="css/design.css" rel="stylesheet" />
+      <link href="css/typography.css" rel="stylesheet" />
     </head>
     <body>
--- index.php
+++ index.php
@@ -1,7 +1,44 @@
 <?php include 'header.php'; ?>
 
-<p>Hello Splashy</p>
+<h1>Bonjour mes chers</h1>
+<h2>Comment allez-vous?</h2>
 
-<p class="ted-rot">Hello Splashy Pants</p>
+<p>J'ai essayé de créer une base typographique. Sentez vous libre de changer <code>typography.less</code> comme vous voulez. Il faudra alors re-génerer <code>typography.css</code> avec <a href="http://wearekiss.com/simpless">Simpless</a> ou <a href="http://incident57.com/less/">Less.app</a> </p>
+
+<ul
+  <li>List item 1</li>
+  <li>List item 2</li>
+  <li>List item 3</li>
+</ul>
+
+<h2>Aliquam lorem ante</h2>
+<h3>Sed in viverra quis</h3>
+<p>dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo.</p>
+
+<h3>Sed fringilla mauris sit amet nibh</h3>
+<p>Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, <strong>mollis</strong> sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris.</p>
+
+<form action="" method="post">
+  <fieldset>
+    <legend>Noms</legend>
+    <label>Prenom :</label>
+    <input type="text" name="prenom" value="" />
+    <label>Nom :</label>
+    <input type="text" name="nom" value="" />
+  </fieldset>
+  <label>Description</label>
+  <textarea name="description"></textarea>
+  <label>Gender</label>
+  Male <input type="radio" class="radio" value="male" name="gender" />
+  Female <input type="radio" class="radio" value="female" name="gender" />
+
+  <label>Tout ok?</label>
+  <input type="checkbox" class="checkbox" value="ok" name="ok" />
+  <fieldset>
+  <legend>Buttons</legend>
+    <input type="submit" name="" value="Submit" />
+    <button name="button">Button</button>
+  </fieldset>
+</form>
 
 <?php include 'footer.php'; ?>
