a0c7879c0acf2af93a61488d406f9d4d884acc5c
Jerome Clot <jerome.clot@heig-vd.ch>
2013-06-14 08:48:10 +0200

    changement css

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php include 'header.php'; ?>
 
-<p>Hello Splashy</p>
+<div class="container">
+<div class="row">
+<p class="span4">Hello Splashy</p>
 
 <p class="ted-rot">Hello Splashy Pants</p>
+
+</div>
+
+</div>
 
 <?php include 'footer.php'; ?>
