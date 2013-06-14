b0c2d6637af4affbbecde7a6efe163388aca6061
Jerome Clot <jerome.clot@heig-vd.ch>
2013-06-14 15:28:17 +0200

    css and stuff

--- footer.php
+++ footer.php
@@ -1,3 +1,6 @@
+	<div class="textFooter">This independently organized TEDx event is operated under license from TED.</div>
+    <div class="trucGrisBas"> </div>
+    </div>   
     <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> -->
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
    </body>
--- index2.php
+++ index2.php
@@ -1,7 +1,7 @@
 <?php include 'header.php'; ?>
 <div class="container"> 
 <?php include 'menu.php';?>
-</div>
+
 
 <?php include 'footer.php'; ?>
 
