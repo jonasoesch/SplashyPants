609148497a1cd89020ce01361dcfe278a10ccc11
Jonas Oesch <jonas.oesch@heig-vd.ch>
2013-06-17 14:03:07 +0200

    Profile Page. Premier essai.

---
+++ profile.php
@@ -0,0 +1,53 @@
+<?php include 'header.php' ?>
+<link href="css/profile.css" rel="stylesheet" />
+<div class="row">
+<h1 class="offset4">
+  <span class="prenom">Christophe</span>
+  <span class="nom">Piscine</span>
+</h1>
+</div>
+
+<div class="row">
+	  <figure class="span2 offset1" >
+	  	<img src="images/profile.jpeg" alt="cristo" class="profile-img" />
+	  </figure>
+	  
+	  <div class="offset1 span8 contact profile-table">
+			
+			<div class="row">
+	  		<p class="span6 email">piscine@villeneuve.ch</p>
+	  		<p class="span6 phone">+41 21 960 19 24</p>
+	  	</div>
+	  	
+	  	<div class="row">
+	  		<p class="span6 address">La plage<br />1844 Villeneuve - CH</p>
+	  		<p class="span6 birthday">13.5.1990</p>
+	  	</div>
+	  	
+	 </div>
+	 
+</div>
+
+<div class="profile-event">
+	<h2>TEDx du 21.5.2011</h2>
+	<div class="row">
+		<h3 class="span4">Registration Status</h3>
+		<p class="span8 accepted">Accepted</p>
+	</div>
+
+<div class="row">
+  <h3 class="span4">Motivation</h3>
+  <p class="span8">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+  </p>
+</div>
+
+<div class="row">
+  <h3 class="span4">Keywords</h3>
+  <ul class="span8">
+    <li>Piscines</li>
+    <li>Baguette</li>
+    <li>90ties Music</li>
+  </ul>
+</div>
+
+<?php include 'footer.php' ?>
