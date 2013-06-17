b1b9854e0fa6b86c08c3b2b872611c9f851ffdde
Jonas Oesch <jonas.oesch@heig-vd.ch>
2013-06-17 10:33:18 +0200

    Login. Premier Essai.

--- login.php
+++ login.php
@@ -1,7 +1,14 @@
 <?php include 'header.php'; ?>
-<form>
-<label for="username">username</label><input type="text" name="username">
-<label for="password">password</label><input type="password" name="password">
-<input type="submit" value="Log In">
-</form>
+<div class="row">
+  <h1 class="offset4">Login</h1>
+</div>
+<div class="row">
+  <form class="span4 offset4">
+    <label for="username">Username</label>
+    <input type="text" name="username">
+    <label for="password">Password</label>
+    <input type="password" name="password">
+    <input type="submit" value="Log In">
+  </form>
+</div>
 <?php include 'footer.php'; ?>
