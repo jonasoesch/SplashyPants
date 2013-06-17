9ef645059c7676c594d386b71f03d85b2a5ead76
Jerome Clot <jerome.clot@heig-vd.ch>
2013-06-17 16:02:38 +0200

    form location

---
+++ formLocation.php
@@ -0,0 +1,23 @@
+<?php 
+	
+?>
+
+
+<form method="post" action="formLocation.php">
+
+
+			<label for="rue">Adress</label>
+	        <input type="text" name="rue" id="rue" />
+	
+       		<label for="numero">Street number</label>
+			<input type="text" name="numero" id="numero" />
+			
+			<label for="postCode">Quel est votre nom ?</label>
+			<input type="text" name="Postcode" id="postCode" />
+			
+			<label for="localite">Quel est votre nom ?</label>
+			<input type="text" name="localite" id="localite" />
+			<input type="submit" value="Envoyer" />
+</form>
+
+
