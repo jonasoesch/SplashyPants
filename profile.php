d9c2582c4f7552ec56eb23e14597bacea1a8750e
Jonas Oesch <jonas.oesch@heig-vd.ch>
2013-06-17 14:38:53 +0200

    Affiche des tags. Du tableau de profile

--- profile.php
+++ profile.php
@@ -1,16 +1,17 @@
 <?php include 'header.php' ?>
 <link href="css/profile.css" rel="stylesheet" />
-<div class="row">
-<h1 class="offset4">
-  <span class="prenom">Christophe</span>
-  <span class="nom">Piscine</span>
-</h1>
-</div>
 
-<div class="row">
+
+<div class="row profile-details">
 	  <figure class="span2 offset1" >
 	  	<img src="images/profile.jpeg" alt="cristo" class="profile-img" />
 	  </figure>
+	  
+
+	  	<h1 class="offset4">
+	  	  <span class="prenom">Christophe</span>
+	  	  <span class="nom">Piscine</span>
+	  	</h1>
 	  
 	  <div class="offset1 span8 contact profile-table">
 			
@@ -44,10 +45,32 @@
 <div class="row">
   <h3 class="span4">Keywords</h3>
   <ul class="span8">
-    <li>Piscines</li>
-    <li>Baguette</li>
-    <li>90ties Music</li>
+    <li class="pill">Piscines</li>
+    <li class="pill">Baguette</li>
+    <li class="pill">90ties Music</li>
   </ul>
 </div>
+
+<div class="profile-event">
+	<h2>TEDx du 11.9.2010</h2>
+	<div class="row">
+		<h3 class="span4">Registration Status</h3>
+		<p class="span8 declined">Declined</p>
+	</div>
+
+<div class="row">
+  <h3 class="span4">Motivation</h3>
+  <p class="span8">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo.
+  </p>
+</div>
+
+<div class="row">
+  <h3 class="span4">Keywords</h3>
+  <ul class="span8">
+    <li class="pill">Cowboys</li>
+    <li class="pill">Pipe Smoking</li>
+  </ul>
+</div>
+
 
 <?php include 'footer.php' ?>
