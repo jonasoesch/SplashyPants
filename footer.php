357003cfac878caf8f2373b60b170d30e64e0311
Jerome Clot <jerome.clot@heig-vd.ch>
2013-06-17 11:17:04 +0200

    sardine

--- footer.php
+++ footer.php
@@ -1,3 +1,14 @@
+            <div class="row">
+                <p class="center">
+                <a href="facebook"><img src="images/facebook.png" /></a>
+                <a href="twitter"><img src="images/twitter.png" /></a>
+                <a href="youtube"><img src="images/youtube.png" /></a>
+                <a href="flickr"><img src="images/flickr.png" /></a>
+                <a href="linkedin"><img src="images/linkedin.png" /></a>
+                <a href="newsletter"><img src="images/newsletter.png" /></a>
+                </p>
+            </div>
+
       <div class="row">
         <p class="textFooter">
           TEDxZurich is a non-profit initiative (Swiss Verein). All content licensed under a CC BY-NC-SA 3.0 license. This independent TEDx event is operated under license from TED.
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
             	<div class="event span6">
                     	<h3 class="span11 offset1"> Discovering Magic Justo</h3>
                     	<h1 class="span11 offset1">02.8.2013</h1>
-                    	<h6 class="span11 offset1"> with Jerome da Clat et Christophe Merkle</h6>
+                    	<h6 class="span11 offset1"> with Jerome da Clat et Christophe Piscine Merkle</h6>
                     	
                     		<a class="span6 offset6" href="moreInfo">more info</a>
 
@@ -12,7 +12,7 @@
                 <div id="video" class="span6">
                 		<figure class="row">
                     	<img src="images/manu-jindal-434px.jpg" class="speakerImage span6"/>
-                    	<p class="speakerText span6"> Manu Jindal: The sky is NOT the limit: live the change <br /> 28.06.2012 <br /> Disruption </p>
+                    	<p class="speakerText span6">Manu Jindal: The sky is NOT the limit: live the change <br /> 28.06.2012 <br /> Disruption </p>
                 		</figure>
                     
                 		<figure class="row">
@@ -27,16 +27,6 @@
                 </div>
             </div>
             
-            <div class="row">
-                <p class="center">
-                <a href="facebook"><img src="images/facebook.png" /></a>
-                <a href="twitter"><img src="images/twitter.png" /></a>
-                <a href="youtube"><img src="images/youtube.png" /></a>
-                <a href="flickr"><img src="images/flickr.png" /></a>
-                <a href="linkedin"><img src="images/linkedin.png" /></a>
-                <a href="newsletter"><img src="images/newsletter.png" /></a>
-                </p>
-            </div>
 
 <?php include 'footer.php'; ?>
 
