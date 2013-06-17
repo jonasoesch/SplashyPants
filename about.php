b4362516dfdf83bfe770a7c0f9ed46f7ff6e49da
Jonas Oesch <jonas.oesch@heig-vd.ch>
2013-06-17 11:01:54 +0200

    About TEDx contenu

--- about.php
+++ about.php
@@ -1,4 +1,10 @@
 <?php include 'header.php'; ?>
-ABOUT
+<h1 class="row">About TEDx</h1>
+<p class="row">
+  In the spirit of “ideas worth spreading”, TED created a program called TEDx, consisting of local, independently organized events that bring people together to share a TED-like experience. Live speakers and a few pre-recorded TEDTalk videos are combined to spark deep discussion and connection in a relatively small group. TED provides general guidance for the TEDx program, including rules and regulations that local organizers must follow. Hundreds of TEDx events have already been organized around the world.
+</p>
 
+<p class="row">
+The TED Conference provides general guidance for the TEDx program, but individual TEDx events, including ours, are self-organized.
+</p>
 <?php include 'footer.php'; ?>
