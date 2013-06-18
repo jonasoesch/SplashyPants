25d76e313aaaf66e68a1911f855bc0bfb327206f
Marine Lemonis <marine.lemonis@heig-vd.ch>
2013-06-18 16:07:47 +0200

    contact us
    page statique formulaire de contact

--- contact.php
+++ contact.php
@@ -1,4 +1,15 @@
 <?php include 'header.php'; ?>
-CONTACT
+<div class="row">
+  <h1 class="offset4">Contact</h1>
+</div>
 
+<div class="row">
+  <form class="span4 offset4">
+    <input type="text" name="name" placeholder="Name" />
+    <input type="text" name="email" placeholder="E-mail" />
+    <input type="text" name="subject" placeholder="Subject" />
+    <textarea name="message" rows="10" placeholder="Message"></textarea>
+    <input type="submit" value="Send">
+  </form>
+</div>
 <?php include 'footer.php'; ?>
