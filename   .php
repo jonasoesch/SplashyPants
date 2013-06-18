e1c4dc278694e861f2172b0c669ef55334e40d40
Christophe Merkle <christophe.merkle@heig-vd.ch>
2013-06-18 16:56:16 +0200

    addparticipator…yes

---
+++   .php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="utf-8">
+      <title>Splashy Title</title>
+      <link href="css/grid.css" rel="stylesheet" />
+      <link href="css/normalize.css" rel="stylesheet" />
+      <link href="css/typography.css" rel="stylesheet" />
+      <link href="css/design.css" rel="stylesheet" />
+
+    </head>
+    <body>
+    	<div class="container">
+<a href="index.php"><img src="images/TEDxlausanne.gif" /></a>
+
+<nav class="menu row">
+	<ul>
+                <li class="span1"><a href="event.php">Event </a></li>
+                <li class="span1"><a href="video.php">Videos </a></li>
+                <li class="span1"><a href="about.php">About </a></li>
+                <li class="span1"><a href="team.php">Team </a></li>
+                <li class="span1"><a href="partner.php">Partner </a></li>
+                <li class="span2"><a href="contact.php">Contact Us </a></li>
+                <div class="span2 offset3" style="align:right;">
+                    <a href="login.php"/>login</a> | <a href="profile-form.php"/>register</a>
+                </div>
+    </ul>
+</nav>
