786d4777c15135ea252e896e6dfec0f4ed58f116
Jerome Clot <jerome.clot@heig-vd.ch>
2013-06-19 11:26:26 +0200

    correction page partner

--- partner.php
+++ partner.php
@@ -1,24 +1,26 @@
 <?php include 'header.php'; ?>
 <div class="row">
-	<h1 class="row">Partners</h1>
+	<h1 class="span2">Partners</h1>
 </div>
 <div class="row">
           <br />
 	  <figure class="span2" >
 	  	<a href="http://www.imd.org/"><img src="images/Logo_IMD2.jpg" alt="logo_IMD2" title="IMD Business School" /></a>
 	  </figure>
-	  <p class= "row offset2"><a href="http://www.imd.org/">IMD Business School</a>, Lausanne, Switzerland is a world pioneer in executive education. More than a business school, they collaborate with individuals, teams and organizations to resolve real business issues, build capabilities and prepare for the future.</p>
-	 	  <br />	
+	  <p class= "span8 offset1"><a href="http://www.imd.org/">IMD Business School</a>, Lausanne, Switzerland is a world pioneer in executive education. More than a business school, they collaborate with individuals, teams and organizations to resolve real business issues, build capabilities and prepare for the future.</p>
+</div>
+
+<div class="row">
   	  <figure class="span2" >
 	  	<a href="http://www.hulihealth.com/"><img src="images/HuliHealth-Logo.png" alt="HuliHealth-Logo" title="hulihealth" /></a>
 	  </figure>
-	  <p class= "row offset2">Our goal in <a href="http://www.hulihealth.com/">HuliHealth</a> is to bring positive disruption to the global healthcare industry by providing transparency and efficiency. With HuliHealth you can search for a top doctor, read his or her profile, communicate, and book an appointment with ease. All the information you need is at your fingertips: your doctor’s credentials, the price of the care you need, reviews from previous patients, and more. At HuliHealth we’re doing everything we can to close the information gap, move towards total transparency in the health care industry, and get you to your best state of health.</p>
-	 	  <br />
+	  <p class= "span8 offset1">Our goal in <a href="http://www.hulihealth.com/">HuliHealth</a> is to bring positive disruption to the global healthcare industry by providing transparency and efficiency. With HuliHealth you can search for a top doctor, read his or her profile, communicate, and book an appointment with ease. All the information you need is at your fingertips: your doctor’s credentials, the price of the care you need, reviews from previous patients, and more. At HuliHealth we’re doing everything we can to close the information gap, move towards total transparency in the health care industry, and get you to your best state of health.</p>
+</div>
+<div class="row"> 	  
 	  <figure class="span2" >
 	  	<a href="http://www.wng.ch/en/"><img src="images/WNG_logo.jpg" alt="WNG-logo" title="WnG Solutions"  /></a>
 	  </figure>
-	  <p class= "row offset2">A 360° web agency that provides innovative strategies and creative solutions for a truly explosive digital marketing approach. <a href="http://www.wng.ch/en/"> WnG Solutions</a> has implemented our website design and provides ever-so-valuable technical support.</p>
-	 	  <br />  
+	  <p class= "span8 offset1">A 360° web agency that provides innovative strategies and creative solutions for a truly explosive digital marketing approach. <a href="http://www.wng.ch/en/"> WnG Solutions</a> has implemented our website design and provides ever-so-valuable technical support.</p>
 </div>
 
 <?php include 'footer.php'; ?>
