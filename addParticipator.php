c2affde93643491d1c52be086f927fc8dea37616
Christophe Merkle <christophe.merkle@heig-vd.ch>
2013-06-19 11:33:47 +0200

    jérômeIsInDaHouse
    Watch your mouth!

--- addParticipator.php
+++ addParticipator.php
@@ -1,11 +1,11 @@
 <?php include 'header.php'; ?>
 <div id="row">
-    <h2 class="span12"> something about love </h2> 
+    <h1 class="span12"> something about love </h1> 
 </div>
 <div id="row">
     <form method="post" action="check.php">
         <div class="span4" >
-            <select class="span6" size="3" name="wannabeParticipants"> 
+            <select class="span10" size="3" name="wannabeParticipants"> 
                 <option VALUE="0"> Kanye West</option>
                 <option VALUE="1"> Diddy</option>
                 <option VALUE="2"> OL'Dirty Bastard</option> 
@@ -16,12 +16,11 @@
             <figure class="span3 ">
                 <img class="profil portrait" src="images/clot.jpg" />
             </figure>
-            <table class="span6 offset2 ">
+            <table class="span7 offset2 ">
                 <tbody>
                     <tr>
                         <td>Hugo Chavez (62)</td>
                     </tr>
-
                     <tr>
                         <td>I would like a Brazzers access card, like ... right now dude. If you don't 
                             let me in this TED i will make something, that you won't forget... for sure. Me and my hombre over there
