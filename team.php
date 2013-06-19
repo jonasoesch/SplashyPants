7f95540f1b17ce5634e007e87d6b823d16c1b6d0
Jerome Clot <jerome.clot@heig-vd.ch>
2013-06-19 10:53:10 +0200

    modif page team

--- team.php
+++ team.php
@@ -1,76 +1,63 @@
 <?php include 'header.php'; ?>
 <link href="css/profile.css" rel="stylesheet" />
-
 <div class="row">
-   <img class="span6 offset3" src="images/group.jpg" />
- 
+	<p><a href="">Curators and hosts</a></p>
+	<p><a href="">Design team</a></p>
+	<p><a href="">External communications team</a></p>
+	<p><a href="">Fundraising & finance team</a></p>
+	<p><a href="">Hospitality & events team</a></p>
+	<p><a href="">Speakers selection team</a></p>
+	<p><a href="">Quality & sustainability team</a></p>
 </div>
-<div class="row event-slot">
-<div class="row">
-          <br />
-	  <figure class="span2" >
-	  	<img src="images/clot.jpg" alt="jerome" class="portrait" />
-	  </figure>
-	  
-	  	<h1 class="offset3">
+
+
+
+	<div class="row event-slot">
+
+		<figure class="span2" >
+	  		<img src="images/clot.jpg" alt="jerome" class="portrait" />
+	  	</figure>
+	  	
+	  	<h1 class="span9 offset1">
 	  	  <span class="prenom">Jerome</span>
 	  	  <span class="nom">Rouf</span>
+	  	  <span class="role">(Bitches coordinator)</span>
 	  	</h1>
-  	  		<p class="span5 offset1 ">Bitches coordinator<br />
-                            <a href="http://www.9gag.com">Jerome.Pimp@heig-vd.ch</a></p>	  			  
-		  	  <br />
-</div>
-</div>
-<div class="row event-slot">
-<div class="row">
-          <br />
+	  	<p class="span9 offset1 ">Originally from eastern Switzerland, he studied engineering at the EPFL in Lausanne and trained in Thessaloniki, Greece and Granada, Spain. More recently, he completed an MBA at the University of Geneva. Simon is in charge of keeping everything and everybody together and on schedule for our TEDxLausanne 2013 event. He currently works as a consultant in construction waste management at Picadus and as quality consultant at Challenge Optimum SA but is always full of new project ideas. Right now, he works on Pages Vertes, a directory of green companies, as well as on Espace Vaud, a space for start-ups, creative people, and non-profit activities. Simon is easily recognized by his unique folding bicycle.<br />
+		<a href="http://www.9gag.com">Jerome.Pimp@heig-vd.ch</a></p>
+     </div>
+
+	<div class="row event-slot">
+          
 	  <figure class="span2" >
 	  	<img src="images/merkle.jpg" alt="merkle" class="portrait" />
 	  </figure>
 	  
-	  	<h1 class="offset3">
-	  	  <span>Bishop Magic</span>
-	  	  <span>Don Christou</span>
-	  	</h1>
-  	  		<p class="span5 offset1 ">Janitorr <br />
-                            <a href="http://www.9gag.com">MagicBishop.Christou@heig-vd.ch</a></p>	  			  
-		  	  <br />
-</div>
-</div>
+	  <h1 class="span9 offset1">
+	  	 <span>Bishop Magic</span>
+	  	 <span>Don Christou</span>
+	  	 <span>(Janitorr)</span>
+	  </h1>
+  	  <p class="span9 offset1 ">Originally from eastern Switzerland, he studied engineering at the EPFL in Lausanne and trained in Thessaloniki, Greece and Granada, Spain. More recently, he completed an MBA at the University of Geneva. Simon is in charge of keeping everything and everybody together and on schedule for our TEDxLausanne 2013 event. He currently works as a consultant in construction waste management at Picadus and as quality consultant at Challenge Optimum SA but is always full of new project ideas. Right now, he works on Pages Vertes, a directory of green companies, as well as on Espace Vaud, a space for start-ups, creative people, and non-profit activities. Simon is easily recognized by his unique folding bicycle.<br />
+		<a href="http://www.9gag.com">Jerome.Pimp@heig-vd.ch</a></p>  			  
+    </div>
+    
     <div class="row event-slot">
-<div class="row">
-          <br />
+    
 	  <figure class="span2" >
 	  	<img src="images/justo2.jpg" alt="justo" class="portrait" />
 	  </figure>
 	  
-	  	<h1 class="offset3">
+	  	<h1 class="span9 offset1 ">
 	  	  <span class="prenom">Muahmar</span>
 	  	  <span class="nom">al Justoaffi</span>
-	  	</h1>
-  	  		<p class="span5 offset1 ">Transport director <br/>
-	  		<a href="http://www.9gag.com">muammar.justoaffe@admin.ch</a></p>	  			  
-		  	  <br />
-</div>
+	  	  <span>(Spanish Artist)</span>
+	  </h1>
+  	  <p class="span9 offset1 ">Originally from eastern Switzerland, he studied engineering at the EPFL in Lausanne and trained in Thessaloniki, Greece and Granada, Spain. More recently, he completed an MBA at the University of Geneva. Simon is in charge of keeping everything and everybody together and on schedule for our TEDxLausanne 2013 event. He currently works as a consultant in construction waste management at Picadus and as quality consultant at Challenge Optimum SA but is always full of new project ideas. Right now, he works on Pages Vertes, a directory of green companies, as well as on Espace Vaud, a space for start-ups, creative people, and non-profit activities. Simon is easily recognized by his unique folding bicycle.<br />
+		<a href="http://www.9gag.com">Jerome.Pimp@heig-vd.ch</a></p> 
+  			  
     </div>
-   <div class="row event-slot">
-<div class="row">
-          <br />
-          <div class="row">
-          <h2 class="span2"> cleaners </h2> </div>
-          <div class="row">
-              <br />
-	  <figure class="span2" >
-	  	<img src="images/justo2.jpg" alt="justo" class="portrait" />
-	  </figure>
-          <figure class="span2" >
-	  	<img src="images/justo2.jpg" alt="justo" class="portrait" />
-	  </figure>
-	  
-          <figure class="span2" >
-	  	<img src="images/justo2.jpg" alt="justo" class="portrait" />
-	  </figure>	  	
+ 	
 </div>
       
-    </div>
 <?php include 'footer.php'; ?>
