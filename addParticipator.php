f29e7655a4a69f4fdeb25d493aaf9c87c4db238c
Christophe Merkle <christophe.merkle@heig-vd.ch>
2013-06-19 11:03:03 +0200

    barbapapa

--- addParticipator.php
+++ addParticipator.php
@@ -1,13 +1,11 @@
 <?php include 'header.php'; ?>
 <div id="row">
     <h2 class="span12"> something about love </h2> 
-
 </div>
 <div id="row">
     <form method="post" action="check.php">
         <div class="span4" >
-
-            <select class="span6"   size="10" name="wannabeParticipants"> 
+            <select class="span6" size="3" name="wannabeParticipants"> 
                 <option VALUE="0"> Kanye West</option>
                 <option VALUE="1"> Diddy</option>
                 <option VALUE="2"> OL'Dirty Bastard</option> 
@@ -15,10 +13,10 @@
             </select>
         </div>
         <div class="span8">
-            <figure class="span2 ">
+            <figure class="span3 ">
                 <img class="profil portrait" src="images/clot.jpg" />
             </figure>
-            <table class="span6 offset2">
+            <table class="span6 offset2 ">
                 <tbody>
                     <tr>
                         <td>Hugo Chavez (62)</td>
@@ -28,25 +26,26 @@
                         <td>I would like a Brazzers access card, like ... right now dude. If you don't 
                             let me in this TED i will make something, that you won't forget... for sure. Me and my hombre over there
                             don't like to play. Gringo!</td>
-
                     </tr>
-
+                     <tr>
+                        <td>SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAGSWAG SWAG SWAG 
+                            SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG SWAG 
+                            SWAG SWAG SWAG SWAG SWAG</td>
+                    </tr>
+                    <tr>
+                        <td>
+                         <input type="Submit" name="add" value="add" /> 
+                         <input type="Submit" name="remove" value="ignore" /> 
+                        </td>
+                    </tr>
                 </tbody>
             </table>
-
-            <input class="span1" type="Submit" name="add" value="add" /> 
-            <input class="span1" type="Submit" name="remove" value="ignore" /> 
-
-            
-        </div>
-
+        </div>    
     </form>
-        <p class="span6 offset3"> 22 participants have been accepted to the (event name) event</p>
 </div>
-
 <div id="row">
-    <p class="span6 offset3"><a href=""> see participatiors list</a> <p>
-
+<p class="span6 offset3"> 22 participants have been accepted to the (event name) event</p>
+<p class="span6 offset3"><a href=""> see participatiors list</a> </p>
 </div>
 </div>
 
