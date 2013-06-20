de802c4c0c71b87cd63a84547ff3c6b604dca074
Jonas Oesch <jonas.oesch@heig-vd.ch>
2013-06-20 12:05:02 +0200

    Rangé

--- controllers/ContactView.php
+++ controllers/ContactView.php
@@ -5,12 +5,6 @@ require_once SPLASHY_DIR."/helpers/Template.php";
 
 class ContactView extends ViewController {
   
-  
-  // ""
-  public function index() {
-   	Template::render('home.tpl');
-  }
-  
   // "contact"
   public function contact() {
   	Template::render('contact.tpl');
--- controllers/TeamView.php
+++ controllers/TeamView.php
@@ -6,11 +6,6 @@ require_once(SPLASHY_DIR."/helpers/Template.php");
 class TeamView extends ViewController {
   
   
-  // ""
-  public function index() {
-   	Template::render('home.tpl');
-  }
-  
    // "Team"
   public function team() {
     Template::render('team.tpl');
