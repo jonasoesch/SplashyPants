e9439650704606bb46bf07ba1b88a1534d5a385d
Jonas Oesch <jonas.oesch@heig-vd.ch>
2013-06-25 15:00:42 +0200

    Router.php pour pingouin1

--- lib/Router.php
+++ lib/Router.php
@@ -31,8 +31,13 @@ class Router {
 	public function __construct() {
 		$request = $this->get_request();
 
-    if(SPLASHY_URL) 
-    {$this->request_uri = str_replace(SPLASHY_URL, "", $request);} 
+    if(SPLASHY_URL) {
+      $this->request_uri = preg_replace(
+        "/^\/.+/",
+        "",
+        str_replace(SPLASHY_URL, "", $request)
+      );
+    } 
     else {$this->request_uri = $request; }
 		$this->routes = array();
 	}
